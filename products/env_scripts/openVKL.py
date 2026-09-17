52c8643a9fa46ebab42fc20a0bc1f07d1fca4f75
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:29 +0200

    chore(openVKL): add environment variable for macos

--- products/env_scripts/openVKL.py
+++ products/env_scripts/openVKL.py
@@ -4,13 +4,17 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
-  env.set('OPENVKL_ROOT_DIR', prereq_dir)
-  env.set('openvkl_DIR', os.path.join(prereq_dir, 'lib','cmake','openvkl-' + version))
-  if platform.system()=="Windows" :
-    env.prepend('PATH', os.path.join(prereq_dir,'bin'))
-  else:
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+    env.set('OPENVKL_ROOT_DIR', prereq_dir)
+    env.set('openvkl_DIR', os.path.join(prereq_dir, 'lib','cmake','openvkl-' + version))
+
+    if platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir,'bin'))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
 
 def set_nativ_env(env):
     pass
-
