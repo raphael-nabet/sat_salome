ef65a50b5d4d48fb46c99915e7c4237f862361bb
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:55 +0200

    chore(LIBBATCH): add environment variable for macos

--- products/env_scripts/LIBBATCH.py
+++ products/env_scripts/LIBBATCH.py
@@ -5,13 +5,18 @@ import platform
 
 def set_env(env, product_dir, version):
     env.set('LIBBATCH_ROOT_DIR', product_dir)
-    
+
     pyver = 'python' + env.get('PYTHON_VERSION')
 
     if platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(product_dir, 'lib'))
         env.prepend('PATH', os.path.join(product_dir, 'lib', pyver))
         env.prepend('PYTHONPATH', os.path.join(product_dir, 'lib', pyver))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(product_dir, 'lib', pyver))
+
     else:
         env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
         env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib', pyver))
