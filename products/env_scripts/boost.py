35ba1a3463669bd4afdd681203fe9d289e2d393e
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:26 +0200

    chore(boost): add environment variable for macos

--- products/env_scripts/boost.py
+++ products/env_scripts/boost.py
@@ -5,27 +5,35 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  if platform.system() == "Windows" :
-    prereq_dir = prereq_dir.replace('/','\\')
-    env.set('BOOSTDIR', prereq_dir)
-    env.set('BOOST_ROOT_DIR', prereq_dir)
-    env.set('BOOST_ROOT', prereq_dir)
-    env.set('BOOST_INCLUDE_DIR',os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
-    env.set('Boost_INCLUDE_DIR',os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
-    env.set('BOOST_INCLUDEDIR', os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
-    env.set('BOOST_VERSION', version)
-    env.set('BOOST_VERSION_MajorMinor', '.'.join(version.split('.')[0:2]))
-    env.set('BOOST_LIBRARY_DIR',os.path.join(prereq_dir,'lib'))
-    env.set('Boost_DIR', os.path.join(prereq_dir,'lib','cmake', 'Boost-' + version))
-    env.set('BOOST_LIBRARYDIR',os.path.join(prereq_dir,'lib'))
-    env.prepend('PATH', os.path.join(prereq_dir, 'lib'))
-  else :
-    env.set('BOOSTDIR', prereq_dir)
-    env.set('BOOST_ROOT_DIR', prereq_dir)
-    env.set('Boost_DIR', os.path.join(prereq_dir,'lib','cmake', 'Boost-' + version))
-    env.prepend('PATH', os.path.join(prereq_dir, 'include'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-        
+    if platform.system() == "Windows" :
+        prereq_dir = prereq_dir.replace('/','\\')
+        env.set('BOOSTDIR', prereq_dir)
+        env.set('BOOST_ROOT_DIR', prereq_dir)
+        env.set('BOOST_ROOT', prereq_dir)
+        env.set('BOOST_INCLUDE_DIR',os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
+        env.set('Boost_INCLUDE_DIR',os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
+        env.set('BOOST_INCLUDEDIR', os.path.join(prereq_dir,'include','boost-' + '_'.join(version.split('.')[0:2])))
+        env.set('BOOST_VERSION', version)
+        env.set('BOOST_VERSION_MajorMinor', '.'.join(version.split('.')[0:2]))
+        env.set('BOOST_LIBRARY_DIR',os.path.join(prereq_dir,'lib'))
+        env.set('Boost_DIR', os.path.join(prereq_dir,'lib','cmake', 'Boost-' + version))
+        env.set('BOOST_LIBRARYDIR',os.path.join(prereq_dir,'lib'))
+        env.prepend('PATH', os.path.join(prereq_dir, 'lib'))
+
+    elif platform.system() == "Darwin" :
+        env.set('BOOSTDIR', prereq_dir)
+        env.set('BOOST_ROOT_DIR', prereq_dir)
+        env.set('Boost_DIR', os.path.join(prereq_dir,'lib','cmake', 'Boost-' + version))
+        env.prepend('PATH', os.path.join(prereq_dir, 'include'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+    else :
+        env.set('BOOSTDIR', prereq_dir)
+        env.set('BOOST_ROOT_DIR', prereq_dir)
+        env.set('Boost_DIR', os.path.join(prereq_dir,'lib','cmake', 'Boost-' + version))
+        env.prepend('PATH', os.path.join(prereq_dir, 'include'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
 def set_nativ_env(env):
   env.set('BOOSTDIR', '/usr')
   env.set('BOOST_ROOT_DIR', '/usr')
