d5d7e5aa8b4455a1f4a403492d910a46c3714ced
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:38 +0200

    chore(expat): add environment variable for macos

--- products/env_scripts/expat.py
+++ products/env_scripts/expat.py
@@ -5,17 +5,23 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('EXPAT_DIR', prereq_dir)
-  env.set('EXPAT_ROOT_DIR', prereq_dir)  # update for cmake  
-  root = env.get('EXPAT_DIR')
-  if platform.system() == "Windows" :
-    env.prepend('INCLUDE', os.path.join(prereq_dir, 'include'))
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
-    env.prepend('LIB', os.path.join(prereq_dir, 'bin'))
-  else :    
-    env.prepend('PATH', os.path.join(root, 'bin'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
+    env.set('EXPAT_DIR', prereq_dir)
+    env.set('EXPAT_ROOT_DIR', prereq_dir)  # update for cmake
+    root = env.get('EXPAT_DIR')
+
+    if platform.system() == "Windows" :
+        env.prepend('INCLUDE', os.path.join(prereq_dir, 'include'))
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('LIB', os.path.join(prereq_dir, 'bin'))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('PATH', os.path.join(root, 'bin'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(root, 'lib'))
+
+    else :
+        env.prepend('PATH', os.path.join(root, 'bin'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
 
 def set_nativ_env(env):
-  env.set('EXPAT_ROOT_DIR', '/usr')  # update for cmake 
-  env.set('EXPAT_DIR', '/usr')
+    env.set('EXPAT_ROOT_DIR', '/usr')  # update for cmake
+    env.set('EXPAT_DIR', '/usr')
