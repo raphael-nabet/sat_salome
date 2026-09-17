6848080e92415de00220fba5ec059ccf37e79540
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:43 +0200

    chore(CDMATH): add environment variable for macos

--- products/env_scripts/CDMATH.py
+++ products/env_scripts/CDMATH.py
@@ -6,17 +6,22 @@ import platform
 
 def set_env(env, prereq_dir, version):
     env.set('CDMATH_ROOT_DIR', prereq_dir)
-    
+
     env.set('CDMATH_DIR', prereq_dir)
 
     root = env.get('CDMATH_DIR')
-    
+
     env.prepend('PATH', os.path.join(root, 'include'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
+    else :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(root, 'lib'))
+
     env.prepend('PYTHONPATH', os.path.join(root, 'lib'))
     env.prepend('PYTHONPATH', os.path.join(root, 'lib', 'cdmath'))
     env.prepend('PYTHONPATH', os.path.join(root, 'bin', 'cdmath'))
 
-        
+
 def set_nativ_env(env):
     pass
