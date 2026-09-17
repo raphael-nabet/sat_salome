57d2eefbb7d0be2b254fb1b8d130c8f725f4809e
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:36 +0200

    chore(embree): add environment variable for macos

--- products/env_scripts/embree.py
+++ products/env_scripts/embree.py
@@ -3,14 +3,17 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('EMBREE_ROOT_DIR', prereq_dir)
-  env.set('EMBREE_VERSION', version)
-  env.set('embree_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'embree-'+version))
-  env.set('EMBREE_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'embree-'+version))
-  if platform.system() == "Windows":
-    env.prepend('PATH',os.path.join(prereq_dir, 'bin'))
-  else:
-    env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    env.set('EMBREE_ROOT_DIR', prereq_dir)
+    env.set('EMBREE_VERSION', version)
+    env.set('embree_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'embree-'+version))
+    env.set('EMBREE_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'embree-'+version))
+
+    if platform.system() == "Windows":
+        env.prepend('PATH',os.path.join(prereq_dir, 'bin'))
+    elif platform.system() == "Darwin":
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    else:
+        env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     pass
