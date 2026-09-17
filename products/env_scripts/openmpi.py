1f1d0cca9687d5d159ba4cee7278ff0b22973ed5
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:35 +0200

    chore(openmpi): add environment variable for macos

--- products/env_scripts/openmpi.py
+++ products/env_scripts/openmpi.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import os.path
+import os.path, platform
 
 def set_env(env, prereq_dir, version):
     env.set('OPENMPIDIR', prereq_dir)
     env.set('OPAL_PREFIX', prereq_dir) # be able to move openmpi install (packages)
-    env.set('MPI_ROOT_DIR', prereq_dir)  # update for cmake  
+    env.set('MPI_ROOT_DIR', prereq_dir)  # update for cmake
     env.set('MPI_ROOT', prereq_dir)
     env.set('MPI_INCLUDE_DIR', os.path.join(prereq_dir, 'include'))
     env.set('MPI_C_COMPILER', os.path.join(prereq_dir, 'bin', 'mpicc'))
@@ -16,7 +16,12 @@ def set_env(env, prereq_dir, version):
     env.set('MPI_C_FOUND', os.path.join(prereq_dir,'lib','libmpi.so'))
     env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
     env.prepend('PATH', os.path.join(prereq_dir, 'include'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
     env.prepend('C_INCLUDE_PATH', os.path.join(prereq_dir, 'include'))  # needed for parallel h5py
     if version in ['4.1.5'] :
         env.set('PMIX_MCA_gds', 'hash')
@@ -51,4 +56,8 @@ def set_nativ_env(env):
     env.set('MPI_INCLUDE_DIR', prereq_inc)
     if prereq_bin != "/usr/bin":
         env.prepend('PATH', prereq_bin)
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+    else :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
