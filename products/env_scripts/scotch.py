c8994258f5419c11d8762cc9375acf22207f1d46
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:05:04 +0200

    chore(scotch): add environment variable for macos

--- products/env_scripts/scotch.py
+++ products/env_scripts/scotch.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import os.path
+import os.path, platform
+
 def set_env(env, prereq_dir, version):
-  SCOTCH_HPC=False
-  #SCOTCH_HPC=env.get('SCOTCH_HPC') == '1'
-  if SCOTCH_HPC: 
-    env.set('SCOTCH_ROOT_DIR', prereq_dir)
-    env.set('PTSCOTCH_ROOT_DIR', prereq_dir)
-    env.set('PTSCOTCHDIR', prereq_dir)
-    env.set('SCOTCHDIR', prereq_dir)
-    env.set('PTSCOTCH_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
-  else:
-    env.set('SCOTCHDIR', prereq_dir)
-    env.set('SCOTCH_ROOT_DIR', prereq_dir)
+    SCOTCH_HPC=False
+    #SCOTCH_HPC=env.get('SCOTCH_HPC') == '1'
+    if SCOTCH_HPC:
+        env.set('SCOTCH_ROOT_DIR', prereq_dir)
+        env.set('PTSCOTCH_ROOT_DIR', prereq_dir)
+        env.set('PTSCOTCHDIR', prereq_dir)
+        env.set('SCOTCHDIR', prereq_dir)
+        env.set('PTSCOTCH_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
+    else:
+        env.set('SCOTCHDIR', prereq_dir)
+        env.set('SCOTCH_ROOT_DIR', prereq_dir)
 
 def set_nativ_env(env):
-  SCOTCH_HPC=False
+    SCOTCH_HPC=False
 
-  #SCOTCH_HPC=env.get('SCOTCH_HPC') == '1'
-  if SCOTCH_HPC:
-    prereq_dir='/usr'
-    prereq_inc='/usr/include'
-    prereq_lib= None
-    try:
-      import distro
-      if any(distribution in distro.name().lower() for distribution in ["rocky", "centos", "fedora"]) :
+    #SCOTCH_HPC=env.get('SCOTCH_HPC') == '1'
+    if SCOTCH_HPC:
         prereq_dir='/usr'
-        prereq_inc= '/usr/include/openmpi-x86_64'
-        prereq_lib='/usr/lib64/openmpi/lib'
-      elif any(distribution in distro.name().lower() for distribution in ["debian", "ubuntu", "tuxedo os", "linux mint"]) :
-        prereq_dir='/usr'
-        prereq_inc='/usr/include/scotch-long'
-        prereq_lib='/usr/lib/x86_64-linux-gnu/scotch-long'
-      else:
-        print("Unimplemented distribution (1): {}".format(distro.name.lower()))
-    except:
-        import platform
-        if any(distribution in platform.linux_distribution()[0].lower() for distribution in ["rocky", "centos", "fedora"]) :
-            prereq_dir='/usr'
-            prereq_inc= '/usr/include/openmpi-x86_64'
-            prereq_lib='/usr/lib64/openmpi/lib'
-        else:
-          print("Unimplemented distribution (2): {}".format(platform.linux_distribution()[0].lower()))
+        prereq_inc='/usr/include'
+        prereq_lib= None
+        try:
+            import distro
+            if any(distribution in distro.name().lower() for distribution in ["rocky", "centos", "fedora"]) :
+                prereq_dir='/usr'
+                prereq_inc= '/usr/include/openmpi-x86_64'
+                prereq_lib='/usr/lib64/openmpi/lib'
+            elif any(distribution in distro.name().lower() for distribution in ["debian", "ubuntu", "tuxedo os", "linux mint"]) :
+                prereq_dir='/usr'
+                prereq_inc='/usr/include/scotch-long'
+                prereq_lib='/usr/lib/x86_64-linux-gnu/scotch-long'
+            else:
+                print("Unimplemented distribution (1): {}".format(distro.name.lower()))
+        except:
+            import platform
+            if any(distribution in platform.linux_distribution()[0].lower() for distribution in ["rocky", "centos", "fedora"]) :
+                prereq_dir='/usr'
+                prereq_inc= '/usr/include/openmpi-x86_64'
+                prereq_lib='/usr/lib64/openmpi/lib'
+            else:
+                print("Unimplemented distribution (2): {}".format(platform.linux_distribution()[0].lower()))
+        env.set('SCOTCH_ROOT_DIR', prereq_dir)
+        env.set('PTSCOTCH_ROOT_DIR', prereq_dir)
+        env.set('PTSCOTCHDIR', prereq_dir)
+        env.set('PTSCOTCH_INCLUDE_DIR', prereq_inc)
 
-    env.set('SCOTCH_ROOT_DIR', prereq_dir)
-    env.set('PTSCOTCH_ROOT_DIR', prereq_dir)
-    env.set('PTSCOTCHDIR', prereq_dir)
-    env.set('PTSCOTCH_INCLUDE_DIR', prereq_inc)
-    if prereq_lib is not None:
-        env.prepend('LD_LIBRARY_PATH', prereq_lib)
-  else:
-    prereq_dir='/usr'
-    env.set('SCOTCH_ROOT_DIR', prereq_dir)
+        if prereq_lib is not None:
+            if platform.system() == "Windows" :
+                pass
+            elif platform.system() == "Darwin" :
+                env.prepend('LD_LIBRARY_PATH', prereq_lib)
+            else :
+                env.prepend('DYLD_LIBRARY_PATH', prereq_lib)
+    else:
+      prereq_dir='/usr'
+      env.set('SCOTCH_ROOT_DIR', prereq_dir)
