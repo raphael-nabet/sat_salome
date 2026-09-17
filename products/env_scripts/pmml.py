62b1eff14f8162b6a417307ef98617eefa6483b9
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:46 +0200

    chore(pmml): add environment variable for macos

--- products/env_scripts/pmml.py
+++ products/env_scripts/pmml.py
@@ -7,13 +7,21 @@ def set_env(env, prereq_dir, version):
     env.set("PMML_ROOT_DIR",prereq_dir)
     env.set('PMML_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
     env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
     if platform.system() == "Windows" :
-      pass
+        pass
+
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        pyver = 'python' + env.get('PYTHON_VERSION')
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
+
     else:
-      env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-      pyver = 'python' + env.get('PYTHON_VERSION')
-      env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
-      env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        pyver = 'python' + env.get('PYTHON_VERSION')
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
 
 def set_nativ_env(env):
     pass
