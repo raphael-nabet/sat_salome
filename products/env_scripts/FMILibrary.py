52594069ec5102867b76834a1e647bbe35f74f1a
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:51 +0200

    chore(FMILibrary): add environment variable for macos

--- products/env_scripts/FMILibrary.py
+++ products/env_scripts/FMILibrary.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import os.path
 import platform
+
 def set_env(env, prereq_dir, version):
     env.set('FMILIBRARY_ROOT_DIR', prereq_dir)
     env.set('FMIL_HOME',prereq_dir)
     env.set('FMILibrary_ROOT',prereq_dir)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
-    if not platform.system() == "Windows" :
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    elif not platform.system() == "Windows" :
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
