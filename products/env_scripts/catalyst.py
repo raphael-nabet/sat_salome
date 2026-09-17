d2929534ef0dc405cb9b0d0234f3054650a34d52
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:28 +0200

    chore(catalyst): add environment variable for macos

--- products/env_scripts/catalyst.py
+++ products/env_scripts/catalyst.py
@@ -5,10 +5,16 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('CATALYST_ROOT_DIR', prereq_dir)
-  env.set('catalyst_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'catalyst-{}'.format(version)))  
-  env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-  env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', 'catalyst'))
+    if platform.system() == "Darwin" :
+        env.set('CATALYST_ROOT_DIR', prereq_dir)
+        env.set('catalyst_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'catalyst-{}'.format(version)))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', 'catalyst'))
+    else:
+        env.set('CATALYST_ROOT_DIR', prereq_dir)
+        env.set('catalyst_DIR', os.path.join(prereq_dir, 'lib', 'cmake', 'catalyst-{}'.format(version)))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', 'catalyst'))
 
 def set_nativ_env(env):
   pass
