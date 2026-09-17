bae0ddf4f87f208269e51b200f0df34363b14f9b
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:47 +0200

    chore(DXF): add environment variable for macos

--- products/env_scripts/DXF.py
+++ products/env_scripts/DXF.py
@@ -10,9 +10,14 @@ def set_env(env, prereq_dir, version):
     env.set('PRODUCTS_DXF_LIBRARIES', os.path.join(prereq_dir, 'lib'))
     env.prepend('PYTHONPATH', env.get('DXF_DIR'))
     env.prepend('PYTHONPATH', env.get('DXF_ROOT_DIR'))
-    
-    if platform.system()=="Windows" :
+
+    if platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(prereq_dir, 'win64', 'vc14' ,'bin'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
     else :
         env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
