c7f1471f679fd4533a9bf7b204e23956e172a1d2
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:17 +0200

    chore(med): add environment variable for macos

--- products/env_scripts/med.py
+++ products/env_scripts/med.py
@@ -7,11 +7,15 @@ def set_env(env, prereq_dir, version):
     env.set('MED3HOME', prereq_dir)
     env.set('MED2HOME', prereq_dir)
     env.set('MEDFILE_ROOT_DIR', prereq_dir)    # update for cmake
-    
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))    
-   
+
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
     if platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(prereq_dir, 'lib'))
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        pyver = 'python' + env.get('PYTHON_VERSION')
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
     else :
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
         pyver = 'python' + env.get('PYTHON_VERSION')
