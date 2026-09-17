015344f5cf75c28f75f48c6f0027ff9e2bb58687
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:57 +0200

    chore(MeshGems): add environment variable for macos

--- products/env_scripts/MeshGems.py
+++ products/env_scripts/MeshGems.py
@@ -6,80 +6,92 @@ import platform
 from distutils.version import LooseVersion
 
 def set_distene_licence(env):
-  try:
-    license_file = env.environ.get_value("LICENCE_FILE")
-  except Exception as e:
+    try:
+        license_file = env.environ.get_value("LICENCE_FILE")
+    except Exception as e:
+        return
+
+    if os.path.exists(license_file):
+        env.add_line(1)
+        env.add_comment("Set DISTENE License")
+        env.set('DISTENE_LICENSE_FILE', 'Use global envvar: DLIM8VAR')
+
+        if os.access(license_file, os.R_OK):
+            lines = open(license_file, "r").readlines()
+            for line in lines:
+                id1=line.find('r"dlim8')
+                if id1 != -1:
+                    # on a trouvé la clé dlim8 dans line, on extrait sa valeur
+                    id2=line.find('"', id1+2)
+                if id2 != -1:
+                    env.set("DLIM8VAR", line[id1+2:id2])
+                    break
     return
-
-  if os.path.exists(license_file):
-    env.add_line(1)
-    env.add_comment("Set DISTENE License")
-    env.set('DISTENE_LICENSE_FILE', 'Use global envvar: DLIM8VAR')
-
-    if os.access(license_file, os.R_OK):
-      lines = open(license_file, "r").readlines()
-      for line in lines:
-        id1=line.find('r"dlim8')
-        if id1 != -1:
-          # on a trouvé la clé dlim8 dans line, on extrait sa valeur
-          id2=line.find('"', id1+2)
-          if id2 != -1:
-           env.set("DLIM8VAR", line[id1+2:id2])
-           break
-  return
 
 def set_DASSAULT_license(env, version):
-  try:
-    license_file_prefix = env.environ.get_value("LICENCE_FILE")
-    linux_dist = env.environ.get_value("sat_dist")
-  except Exception as e:
+    try:
+        license_file_prefix = env.environ.get_value("LICENCE_FILE")
+        linux_dist = env.environ.get_value("sat_dist")
+    except Exception as e:
+        return
+
+    env.add_comment("DASSAULT MeshGems KeyGenerator based License")
+
+    if platform.system() == "Windows" :
+        license_file_name=license_file_prefix + '-' + version + '.dll'
+
+    if platform.system() == "Darwin" :
+        license_file_name=license_file_prefix + '-' + version + '-' + linux_dist + '.dylib'
+
+    else:
+        license_file_name=license_file_prefix + '-' + version + '-' + linux_dist + '.so'
+
+    if not os.path.exists(license_file_name):
+        print("\nWARNING : DASSAULT license file %s not found!" % license_file_name) 
+
+    env.set('SALOME_MG_KEYGEN_LIB_PATH', license_file_name)
     return
-  env.add_comment("DASSAULT MeshGems KeyGenerator based License")
-  if platform.system() == "Windows" :
-    license_file_name=license_file_prefix + '-' + version + '.dll'
-  else:
-    license_file_name=license_file_prefix + '-' + version + '-' + linux_dist + '.so'
-
-  if not os.path.exists(license_file_name):
-     print("\nWARNING : DASSAULT license file %s not found!" % license_file_name) 
-
-  env.set('SALOME_MG_KEYGEN_LIB_PATH', license_file_name)
-  return
 
 def set_env_build(env, prereq_dir, version):
-  env.set('MESHGEMSHOME', prereq_dir)
-  env.set('MESHGEMS_ROOT_DIR', prereq_dir)    # update for cmake
+    env.set('MESHGEMSHOME', prereq_dir)
+    env.set('MESHGEMS_ROOT_DIR', prereq_dir)    # update for cmake
 
-  env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
 
-  if platform.system() == "Windows" :
-    env.prepend('PATH', os.path.join(prereq_dir, 'lib'))
-  else :
-    libdir = "Linux_64"
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin', libdir))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', libdir))
+    if platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'lib'))
+
+    elif platform.system() == "Darwin" :
+        libdir = "macOS_arm64" # need review
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin', libdir))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', libdir))
+
+    else :
+        libdir = "Linux_64"
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin', libdir))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', libdir))
 
 def set_env(env, prereq_dir, version):
-  env.add_comment("Here you can define your license parameters for MeshGems")
-  if LooseVersion(version) > LooseVersion('2.12-1'):
-    set_DASSAULT_license(env,version)
-  else:
-    env.add_comment("DISTENE license")
-    if not env.forBuild:
-      # we don't need licence keys at compile time
-      set_distene_licence(env)
-  set_env_build(env, prereq_dir, version)
-  #
-  Mmp = list(map(str, version.replace('-','.').split('.')))
-  M,m=Mmp[0:2]
-  if len(Mmp) > 2:
-    p=Mmp[2]
-  else:
-    p='0'
-  env.set('MESHGEMS_VERSION', version)
-  env.set('MESHGEMS_VERSION_MAJOR',M)
-  env.set('MESHGEMS_VERSION_MINOR',m)
-  env.set('MESHGEMS_VERSION_PATCH',p)
+    env.add_comment("Here you can define your license parameters for MeshGems")
+    if LooseVersion(version) > LooseVersion('2.12-1'):
+        set_DASSAULT_license(env,version)
+    else:
+        env.add_comment("DISTENE license")
+        if not env.forBuild:
+            # we don't need licence keys at compile time
+            set_distene_licence(env)
+    set_env_build(env, prereq_dir, version)
+    #
+    Mmp = list(map(str, version.replace('-','.').split('.')))
+    M,m=Mmp[0:2]
+    if len(Mmp) > 2:
+        p=Mmp[2]
+    else:
+        p='0'
+    env.set('MESHGEMS_VERSION', version)
+    env.set('MESHGEMS_VERSION_MAJOR',M)
+    env.set('MESHGEMS_VERSION_MINOR',m)
+    env.set('MESHGEMS_VERSION_PATCH',p)
 
 def set_nativ_env(env):
-  pass
+    pass
