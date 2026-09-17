51cf2ae4ba2f6f8b8cd72028b5ae6b04d3dea39f
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:32 +0200

    chore(cminpack): add environment variable for macos

--- products/env_scripts/cminpack.py
+++ products/env_scripts/cminpack.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
+
 import os.path
 import platform
+
 def set_env(env, prereq_dir, version):
     env.set('CMINPACK_ROOT_DIR', prereq_dir)
     env.set('CMINPACK_DIR', os.path.join(prereq_dir,'share','cmake','CMinpack'))
     env.set('CMinpack_DIR', os.path.join(prereq_dir,'share','cmake','CMinpack'))
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
-    if not platform.system() == "Windows" :
+
+    if platform.system() == "Windows" :
+        pass
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    else:
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
