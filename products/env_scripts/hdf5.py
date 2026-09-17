ed8cd638ef7dcbc0bce9416625da40cb2ba686f9
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:01 +0200

    chore(hdf5): add environment variable for macos

--- products/env_scripts/hdf5.py
+++ products/env_scripts/hdf5.py
@@ -15,11 +15,14 @@ def set_env(env, prereq_dir, version):
     env.set('HDF5_INCLUDE_DIRS', os.path.join(prereq_dir, 'include'))
     env.set('HDF5_VERSION', version)
 
-    if not platform.system() == "Windows" :
+    if platform.system() == "Windows" :
+        pass
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else:
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    
+
 def set_nativ_env(env):
     env.set('HDF5HOME', '/usr')
     env.set('HDF5_ROOT', '/usr')
     env.set('HDF5_ROOT_DIR', '/usr')    # update for cmake
-
