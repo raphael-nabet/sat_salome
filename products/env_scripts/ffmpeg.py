a3a731e935e78f596490326e094432e9396da02a
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:40 +0200

    chore(ffmpeg): add environment variable for macos

--- products/env_scripts/ffmpeg.py
+++ products/env_scripts/ffmpeg.py
@@ -4,12 +4,18 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
-  if platform.system() == "Windows":
-    pass
-  else:
-    env.set("FFMPEG_ROOT_DIR",prereq_dir)
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.prepend('PKG_CONFIG_PATH',os.path.join(prereq_dir, 'lib', 'pkgconfig'))
+    if platform.system() == "Windows":
+        pass
+
+    elif platform.system() == "Darwin":
+        env.set("FFMPEG_ROOT_DIR",prereq_dir)
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('PKG_CONFIG_PATH',os.path.join(prereq_dir, 'lib', 'pkgconfig'))
+
+    else:
+        env.set("FFMPEG_ROOT_DIR",prereq_dir)
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('PKG_CONFIG_PATH',os.path.join(prereq_dir, 'lib', 'pkgconfig'))
 
 def set_nativ_env(env):
   pass
