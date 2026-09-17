6751136b071f17fc3f7a0e3834fe41af2a06a10a
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:38 +0200

    chore(openturns): add environment variable for macos

--- products/env_scripts/openturns.py
+++ products/env_scripts/openturns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 import os.path
 import platform
+
 def set_env(env, prereq_dir, version):
     env.set('OT_ROOT_DIR', prereq_dir)
     env.set('OPENTURNS_HOME', prereq_dir)
@@ -8,8 +10,15 @@ def set_env(env, prereq_dir, version):
     env.set('OT_VERSION',version)
     env.set('OPENTURNS_CONFIG_PATH', os.path.join(prereq_dir,'etc','openturns'))
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
+
     if platform.system() == "Windows" :
         env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib','site-packages'))
+
+    elif platform.system() == "Darwin" :
+        pyver = 'python' + env.get('PYTHON_VERSION')
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+
     else:
         pyver = 'python' + env.get('PYTHON_VERSION')
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
