b2a745333ac8e86f6af90c0278a73c5fc2e51ab7
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:49 +0200

    chore(gcc): add environment variable for macos

--- products/env_scripts/gcc.py
+++ products/env_scripts/gcc.py
@@ -4,18 +4,32 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
-  if not platform.system() == "Windows" :
-    env.set('CC', os.path.join(prereq_dir,'bin','gcc'))
-    env.set('CXX', os.path.join(prereq_dir,'bin','g++'))
-    env.set('FC', os.path.join(prereq_dir,'bin','gfortran'))
-    env.set('EBROOTGCC', version)
-    env.set('EBVERSIONGCC', version)
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
-    env.prepend('LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
-    env.prepend('MANPATH', os.path.join(prereq_dir, 'share','man'))
-    env.prepend('INCLUDE', os.path.join(prereq_dir, 'include'))
-    env.prepend('CPATH', os.path.join(prereq_dir, 'include'))
+    if platform.system() == "Windows" :
+        pass
+    elif platform.system() == "Darwin" :
+        env.set('CC', os.path.join(prereq_dir,'bin','gcc'))
+        env.set('CXX', os.path.join(prereq_dir,'bin','g++'))
+        env.set('FC', os.path.join(prereq_dir,'bin','gfortran'))
+        env.set('EBROOTGCC', version)
+        env.set('EBVERSIONGCC', version)
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
+        env.prepend('LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
+        env.prepend('MANPATH', os.path.join(prereq_dir, 'share','man'))
+        env.prepend('INCLUDE', os.path.join(prereq_dir, 'include'))
+        env.prepend('CPATH', os.path.join(prereq_dir, 'include'))
+    else:
+        env.set('CC', os.path.join(prereq_dir,'bin','gcc'))
+        env.set('CXX', os.path.join(prereq_dir,'bin','g++'))
+        env.set('FC', os.path.join(prereq_dir,'bin','gfortran'))
+        env.set('EBROOTGCC', version)
+        env.set('EBVERSIONGCC', version)
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
+        env.prepend('LIBRARY_PATH', os.path.join(prereq_dir, 'lib64'))
+        env.prepend('MANPATH', os.path.join(prereq_dir, 'share','man'))
+        env.prepend('INCLUDE', os.path.join(prereq_dir, 'include'))
+        env.prepend('CPATH', os.path.join(prereq_dir, 'include'))
 
 def set_nativ_env(env):
-  pass
+    pass
