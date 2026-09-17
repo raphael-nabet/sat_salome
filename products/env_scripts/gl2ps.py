b9680e01138b3924e807f845fc7a2702c25671c8
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:53 +0200

    chore(gl2ps): add environment variable for macos

--- products/env_scripts/gl2ps.py
+++ products/env_scripts/gl2ps.py
@@ -5,13 +5,16 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('GL2PSDIR', prereq_dir)
-  env.set('GL2PS_ROOT_DIR', prereq_dir)
-    
-  env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    env.set('GL2PSDIR', prereq_dir)
+    env.set('GL2PS_ROOT_DIR', prereq_dir)
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
 
-  if not platform.system() == "Windows" :
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    if platform.system() == "Windows" :
+        pass
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
-  pass
+    pass
