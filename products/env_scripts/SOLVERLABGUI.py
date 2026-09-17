b8b563a748525735e286d8f7a0d1cc1c763a9a87
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:16 +0200

    chore(SOLVERLABGUI): add environment variable for macos

--- products/env_scripts/SOLVERLABGUI.py
+++ products/env_scripts/SOLVERLABGUI.py
@@ -5,13 +5,22 @@ import os.path
 import platform
 
 def set_env(env, product_dir, version):
-    env.set('SOLVERLABGUI_ROOT_DIR', product_dir)    
-    env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
-    env.prepend('PYTHONPATH', product_dir)
-    env.prepend('PYTHONPATH', os.path.join(product_dir, 'solverlabpy'))
-    env.prepend('PYTHONPATH', os.path.join(product_dir, 'bin', 'salome'))
-    env.set('SOLVERLABGUI_WORKDIR', r'/tmp')
-    env.set('USERLOGIN', "$USER")
+    if platform.system() == "Darwin" :
+        env.set('SOLVERLABGUI_ROOT_DIR', product_dir)
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+        env.prepend('PYTHONPATH', product_dir)
+        env.prepend('PYTHONPATH', os.path.join(product_dir, 'solverlabpy'))
+        env.prepend('PYTHONPATH', os.path.join(product_dir, 'bin', 'salome'))
+        env.set('SOLVERLABGUI_WORKDIR', r'/tmp')
+        env.set('USERLOGIN', "$USER")
+    else:
+        env.set('SOLVERLABGUI_ROOT_DIR', product_dir)
+        env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+        env.prepend('PYTHONPATH', product_dir)
+        env.prepend('PYTHONPATH', os.path.join(product_dir, 'solverlabpy'))
+        env.prepend('PYTHONPATH', os.path.join(product_dir, 'bin', 'salome'))
+        env.set('SOLVERLABGUI_WORKDIR', r'/tmp')
+        env.set('USERLOGIN', "$USER")
  
 def set_nativ_env(env):
     pass
