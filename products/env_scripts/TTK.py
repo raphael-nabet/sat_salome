11a086bc4be981ade2fb7d20cf89bbfc5213c71f
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:20 +0200

    chore(TTK): add environment variable for macos

--- products/env_scripts/TTK.py
+++ products/env_scripts/TTK.py
@@ -7,10 +7,18 @@ def set_env(env, prereq_dir, version):
     env.set('TTK_HOME', prereq_dir)
     env.set('TTK_VERSION',version)
     pyver = 'python' + env.get('PYTHON_VERSION')
+
     if platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(prereq_dir,'bin'))
         env.prepend('PV_PLUGIN_PATH', os.path.join(prereq_dir, 'bin'))
         env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('PATH', os.path.join(prereq_dir,'bin'))
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+        env.prepend('PV_PLUGIN_PATH',os.path.join(prereq_dir,'bin','plugins'))
+
     else:
         env.prepend('PATH', os.path.join(prereq_dir,'bin'))
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
