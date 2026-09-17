25ed1c64709ebf42143060a92650ba95fd633c45
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:06 +0200

    chore(ParMetis): add environment variable for macos

--- products/env_scripts/ParMetis.py
+++ products/env_scripts/ParMetis.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import os.path
+import os.path, platform
 
 def set_env(env, prereq_dir, version):
     env.set('PARMETISDIR', prereq_dir)
     env.set('PARMETIS_ROOT_DIR', prereq_dir)   # update for cmake 
     root = env.get('PARMETISDIR')
 
-    env.prepend('LD_LIBRARY_PATH', root)
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', root)
+    else :
+        env.prepend('LD_LIBRARY_PATH', root)
     env.prepend('PATH', root)
 #    env.prepend('PATH', os.path.join(root, "Graphs"))
 
