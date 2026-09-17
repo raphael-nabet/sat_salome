593fe2be5c22f1d66177c4e46555ae60d41d90f4
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:12 +0200

    chore(SALOMEBOOTSTRAP): add environment variable for macos

--- products/env_scripts/SALOMEBOOTSTRAP.py
+++ products/env_scripts/SALOMEBOOTSTRAP.py
@@ -6,18 +6,20 @@ import platform
 import re
 
 def set_env(env, prereq_dir, version):
-  pyver = 'python' + env.get('PYTHON_VERSION')
-  env.set('SALOMEBOOTSTRAP_ROOT_DIR', prereq_dir)
-  env.set('SALOME_APPLICATION_DIR', prereq_dir)
-  env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__SALOME_BOOTSTRAP__'))
-  if re.match(r'^V9_[1][0-6]_([a-zA-Z0-9]+)$', version):
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
-  else:
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', pyver, 'site-packages'))
-  if platform.system() == "Windows" :
-    env.prepend('PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
-  else:
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+    pyver = 'python' + env.get('PYTHON_VERSION')
+    env.set('SALOMEBOOTSTRAP_ROOT_DIR', prereq_dir)
+    env.set('SALOME_APPLICATION_DIR', prereq_dir)
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__SALOME_BOOTSTRAP__'))
+    if re.match(r'^V9_[1][0-6]_([a-zA-Z0-9]+)$', version):
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+    else:
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', pyver, 'site-packages'))
+    if platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
 
 def set_nativ_env(env):
-  pass
+    pass
