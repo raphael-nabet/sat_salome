ca50be2f2541ea63c4ca0af6e9539673bd76cf6f
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:57 +0200

    chore(gmsh): add environment variable for macos

--- products/env_scripts/gmsh.py
+++ products/env_scripts/gmsh.py
@@ -5,19 +5,25 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version): 
-  env.set('GMSHHOME', prereq_dir)
-  env.set('GMSH_ROOT_DIR', prereq_dir)
-  env.set('GMSH_VERSION', version)
-  env.append('PATH',os.path.join(prereq_dir,'bin'))
-  if platform.system() == "Windows" :
-    env.set('GMSH_DIR', os.path.join(prereq_dir, 'share', 'gmsh'))
-    env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include'))
-    env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include', 'gmsh'))
-    env.set('GMSH_LIBRARIES', os.path.join(prereq_dir, 'bin', 'gmsh.lib'))
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
-  else:
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib'))
+    env.set('GMSHHOME', prereq_dir)
+    env.set('GMSH_ROOT_DIR', prereq_dir)
+    env.set('GMSH_VERSION', version)
+    env.append('PATH',os.path.join(prereq_dir,'bin'))
+
+    if platform.system() == "Windows" :
+        env.set('GMSH_DIR', os.path.join(prereq_dir, 'share', 'gmsh'))
+        env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include'))
+        env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include', 'gmsh'))
+        env.set('GMSH_LIBRARIES', os.path.join(prereq_dir, 'bin', 'gmsh.lib'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     env.set('GMSH_ROOT_DIR', '/usr')
