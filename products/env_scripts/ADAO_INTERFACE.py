950059ad5bbd66539444225c22d07a1b46fb5c6e
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:39 +0200

    chore(ADAO_INTERFACE): add environment variable for macos

--- products/env_scripts/ADAO_INTERFACE.py
+++ products/env_scripts/ADAO_INTERFACE.py
@@ -4,8 +4,12 @@ import os
 import platform
 
 def set_env(env, product_dir, version):
-    env.set('ADAO_INTERFACE_ROOT_DIR', product_dir)
-    env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+    if platform.system() == "Darwin" :
+        env.set('ADAO_INTERFACE_ROOT_DIR', product_dir)
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+    else :
+        env.set('ADAO_INTERFACE_ROOT_DIR', product_dir)
+        env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
 
 def set_nativ_env(env):
     pass
