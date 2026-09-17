49a52b77604504a37a27f81e184e545ba9a061fb
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:05:00 +0200

    chore(rkCommon): add environment variable for macos

--- products/env_scripts/rkCommon.py
+++ products/env_scripts/rkCommon.py
@@ -4,13 +4,17 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
-  env.set('RKCOMMON_ROOT_DIR', prereq_dir)
-  env.set('rkcommon_DIR', os.path.join(prereq_dir, 'lib','cmake','rkcommon-'+ version))
-  if platform.system()=="Windows" :
-    env.prepend('PATH', os.path.join(prereq_dir,'bin'))
-  else:
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+    env.set('RKCOMMON_ROOT_DIR', prereq_dir)
+    env.set('rkcommon_DIR', os.path.join(prereq_dir, 'lib','cmake','rkcommon-'+ version))
+
+    if platform.system()=="Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir,'bin'))
+
+    elif platform.system()=="Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
 
 def set_nativ_env(env):
     pass
-
