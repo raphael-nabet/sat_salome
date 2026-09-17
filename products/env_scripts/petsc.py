266f62905854d0db55cd9ca1d69c721dfeada757
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:42 +0200

    chore(petsc): add environment variable for macos

--- products/env_scripts/petsc.py
+++ products/env_scripts/petsc.py
@@ -5,24 +5,34 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('PETSCDIR', prereq_dir)
-  env.set('PETSC_ROOT_DIR', prereq_dir)
-  env.set('PETSC_DIR', prereq_dir)
-  env.set('PETSC4PY', os.path.join(prereq_dir, 'lib'))
-  env.set('SLEPC4PY', os.path.join(prereq_dir, 'lib'))
+    env.set('PETSCDIR', prereq_dir)
+    env.set('PETSC_ROOT_DIR', prereq_dir)
+    env.set('PETSC_DIR', prereq_dir)
+    env.set('PETSC4PY', os.path.join(prereq_dir, 'lib'))
+    env.set('SLEPC4PY', os.path.join(prereq_dir, 'lib'))
 
-  if platform.system() == "Windows" :
-    pass
-  else:
-    env.set('PETSC_ARCH', 'arch-linux-c-opt')
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    # Setting pythonpath for libraries petsc4py and slepc4py
-    petsc4y=env.get('PETSC4PY')
-    slepc4y=env.get('SLEPC4PY')
-    env.prepend('PYTHONPATH', petsc4y)
-    env.prepend('PYTHONPATH', os.path.join(petsc4y, 'lib'))
-    env.prepend('PYTHONPATH', slepc4y)
-    env.prepend('PYTHONPATH', os.path.join(slepc4y, 'lib'))
+    if platform.system() == "Windows" :
+        pass
+    elif platform.system() == "Darwin" :
+        env.set('PETSC_ARCH', 'arch-linux-c-opt')
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # Setting pythonpath for libraries petsc4py and slepc4py
+        petsc4y=env.get('PETSC4PY')
+        slepc4y=env.get('SLEPC4PY')
+        env.prepend('PYTHONPATH', petsc4y)
+        env.prepend('PYTHONPATH', os.path.join(petsc4y, 'lib'))
+        env.prepend('PYTHONPATH', slepc4y)
+        env.prepend('PYTHONPATH', os.path.join(slepc4y, 'lib'))
+    else:
+        env.set('PETSC_ARCH', 'arch-linux-c-opt')
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # Setting pythonpath for libraries petsc4py and slepc4py
+        petsc4y=env.get('PETSC4PY')
+        slepc4y=env.get('SLEPC4PY')
+        env.prepend('PYTHONPATH', petsc4y)
+        env.prepend('PYTHONPATH', os.path.join(petsc4y, 'lib'))
+        env.prepend('PYTHONPATH', slepc4y)
+        env.prepend('PYTHONPATH', os.path.join(slepc4y, 'lib'))
 
 def set_nativ_env(env):
   pass
