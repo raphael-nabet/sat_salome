3a0daf2853a3a111498be08e05c3c381663c8bb2
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:05 +0200

    chore(jsoncpp): add environment variable for macos

--- products/env_scripts/jsoncpp.py
+++ products/env_scripts/jsoncpp.py
@@ -5,16 +5,20 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('JSONCPP_ROOT_DIR', prereq_dir)
-  env.set('JsonCpp_DIR', os.path.join(prereq_dir,'lib','cmake', 'jsoncpp'))
-  env.set('JsonCpp_INCLUDE_DIR', os.path.join(prereq_dir,'include'))
-  env.set('JsonCpp_LIBRARY', os.path.join(prereq_dir,'lib'))
-    
-  if not platform.system() == "Windows" :
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-  else:
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    env.set('JSONCPP_ROOT_DIR', prereq_dir)
+    env.set('JsonCpp_DIR', os.path.join(prereq_dir,'lib','cmake', 'jsoncpp'))
+    env.set('JsonCpp_INCLUDE_DIR', os.path.join(prereq_dir,'include'))
+    env.set('JsonCpp_LIBRARY', os.path.join(prereq_dir,'lib'))
+
+    if platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     env.set('JSONCPP_ROOT_DIR', '/usr')
