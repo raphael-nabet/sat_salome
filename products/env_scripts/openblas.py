08c22944dd7757796bae392038b3cdc05ac58817
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:31 +0200

    chore(openblas): add environment variable for macos

--- products/env_scripts/openblas.py
+++ products/env_scripts/openblas.py
@@ -5,23 +5,33 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('OPENBLASHOME', prereq_dir)
-  env.set('OPENBLAS_ROOT_DIR', prereq_dir)
-  env.set('BLAS_ROOT_DIR', prereq_dir)
-  env.set('OPENBLAS_SRC', os.path.join(prereq_dir,'SRC'))
-  env.set('BLAS_SRC', os.path.join(prereq_dir,'BLAS','SRC'))
+    env.set('OPENBLASHOME', prereq_dir)
+    env.set('OPENBLAS_ROOT_DIR', prereq_dir)
+    env.set('BLAS_ROOT_DIR', prereq_dir)
+    env.set('OPENBLAS_SRC', os.path.join(prereq_dir,'SRC'))
+    env.set('BLAS_SRC', os.path.join(prereq_dir,'BLAS','SRC'))
 
-  if not platform.system() == "Windows" :
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    # FOR NUMPY AND SCIPY
-    env.set('BLAS', os.path.join(prereq_dir, 'lib'))
-    env.set('OPENBLAS', os.path.join(prereq_dir, 'lib'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.set('OpenBLAS_DIR', os.path.join(prereq_dir, 'lib','cmake','OpenBLAS'))
-    env.prepend('PKG_CONFIG_PATH', os.path.join(prereq_dir, 'lib', 'pkgconfig'))
-  else:
-    env.set('OpenBLAS_DIR', os.path.join(prereq_dir, 'share','cmake','OpenBLAS'))
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    if platform.system() == "Windows" :
+        env.set('OpenBLAS_DIR', os.path.join(prereq_dir, 'share','cmake','OpenBLAS'))
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
+    elif not platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # FOR NUMPY AND SCIPY
+        env.set('BLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('OPENBLAS', os.path.join(prereq_dir, 'lib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.set('OpenBLAS_DIR', os.path.join(prereq_dir, 'lib','cmake','OpenBLAS'))
+        env.prepend('PKG_CONFIG_PATH', os.path.join(prereq_dir, 'lib', 'pkgconfig'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # FOR NUMPY AND SCIPY
+        env.set('BLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('OPENBLAS', os.path.join(prereq_dir, 'lib'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.set('OpenBLAS_DIR', os.path.join(prereq_dir, 'lib','cmake','OpenBLAS'))
+        env.prepend('PKG_CONFIG_PATH', os.path.join(prereq_dir, 'lib', 'pkgconfig'))
 
 def set_nativ_env(env):
     env.set('OPENBLASHOME', '/usr')
