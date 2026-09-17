b2352c0b4ea4a963d7d3ed8f660df1abcde2743b
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:24 +0200

    chore(bison): add environment variable for macos

--- products/env_scripts/bison.py
+++ products/env_scripts/bison.py
@@ -5,15 +5,15 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  if platform.system() == "Darwin" :
-    env.set('BISONDIR', prereq_dir)
-    env.set('BISON_ROOT_DIR', prereq_dir)
-    env.set('Bison_DIR', os.path.join(prereq_dir,'lib', 'Bison-' + version))
-    env.prepend('PATH', os.path.join(prereq_dir, 'include'))
-    env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    if platform.system() == "Darwin" :
+        env.set('BISONDIR', prereq_dir)
+        env.set('BISON_ROOT_DIR', prereq_dir)
+        env.set('Bison_DIR', os.path.join(prereq_dir,'lib', 'Bison-' + version))
+        env.prepend('PATH', os.path.join(prereq_dir, 'include'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     env.set('BISONROOT', '/usr')
     env.set('BISON_ROOT_DIR', '/usr')
     if platform.system() == "Darwin" :
-      env.prepend('PATH', '/opt/homebrew/opt/bison/bin')
+        env.prepend('PATH', '/opt/homebrew/opt/bison/bin')
