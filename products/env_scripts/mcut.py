4a26a86ffa904114da4ce508b133ca7f5e4035c0
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:15 +0200

    chore(mcut): add environment variable for macos

--- products/env_scripts/mcut.py
+++ products/env_scripts/mcut.py
@@ -5,10 +5,14 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('MCUT_ROOT_DIR', prereq_dir)
-  env.set('MCUT_VERSION', version)
-  env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
-  env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    env.set('MCUT_ROOT_DIR', prereq_dir)
+    env.set('MCUT_VERSION', version)
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
-  pass
+    pass
