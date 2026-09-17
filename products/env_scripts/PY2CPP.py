0c260343f6dc163bcb99c6b1bacfca6ed3f9e588
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:04 +0200

    chore(PY2CPP): add environment variable for macos

--- products/env_scripts/PY2CPP.py
+++ products/env_scripts/PY2CPP.py
@@ -6,10 +6,15 @@ import platform
 def set_env(env, product_dir, version):
     env.set('PY2CPP_ROOT_DIR', product_dir)
     env.set('Py2cpp_DIR', os.path.join(product_dir, 'lib', 'cmake', 'py2cpp'))
-    if not platform.system() == "Windows" :
-        env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
-    else:
+
+    if platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(product_dir, 'bin'))
+
+    elif not platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
 
 def set_nativ_env(env):
     pass
