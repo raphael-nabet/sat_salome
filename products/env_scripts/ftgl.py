bafe62304ac97ea150f431afe2fe41879d4066ee
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:46 +0200

    chore(ftgl): add environment variable for macos

--- products/env_scripts/ftgl.py
+++ products/env_scripts/ftgl.py
@@ -2,15 +2,25 @@
 #-*- coding:utf-8 -*-
 
 import os.path
+import platform
 
 def set_env(env, prereq_dir, version):
     env.set('FTGLDIR', prereq_dir)
     env.set('FTGL_ROOT_DIR', prereq_dir)    # update for cmake
     root = env.get('FTGLDIR')
-    
-    env.prepend('PATH', os.path.join(root, 'bin'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
-    #env.set('LD_PRELOAD', os.path.join(root, 'lib', 'libftgl.so'))
+
+    if platform.system() == "Windows":
+        pass
+
+    elif platform.system() == "Darwin":
+        env.prepend('PATH', os.path.join(root, 'bin'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(root, 'lib'))
+
+    else :
+        env.prepend('PATH', os.path.join(root, 'bin'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
+
+        #env.set('LD_PRELOAD', os.path.join(root, 'lib', 'libftgl.so'))
 
 def set_nativ_env(env):
     env.set('FTGL_ROOT_DIR', '/usr')    # update for cmake
