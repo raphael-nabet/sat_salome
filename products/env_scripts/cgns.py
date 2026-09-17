74034051c9a4fccc97ee156fe891f1e56acd0663
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:30 +0200

    chore(cgns): add environment variable for macos

--- products/env_scripts/cgns.py
+++ products/env_scripts/cgns.py
@@ -4,14 +4,20 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
-  env.set('CGNS_ROOT_DIR', prereq_dir)
-  env.set('CGNS_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
-  env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
-  if platform.system() == "Windows" :
-    env.set('CGNS_LIBRARY',os.path.join(prereq_dir,'lib','libcgnsdll.lib'))
-  else:
-    env.set('CGNS_LIBRARY',os.path.join(prereq_dir,'lib','libcgns.so'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    env.set('CGNS_ROOT_DIR', prereq_dir)
+    env.set('CGNS_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
+    if platform.system() == "Windows" :
+        env.set('CGNS_LIBRARY',os.path.join(prereq_dir,'lib','libcgnsdll.lib'))
+
+    elif platform.system() == "Darwin" :
+        env.set('CGNS_LIBRARY',os.path.join(prereq_dir,'lib','libcgns.dylib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+    else:
+        env.set('CGNS_LIBRARY',os.path.join(prereq_dir,'lib','libcgns.so'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
-  env.set('CGNS_ROOT_DIR', '/usr')
+    env.set('CGNS_ROOT_DIR', '/usr')
