5c6ada6b5563d12bc9e3f9e2808bcc5d783279c6
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:03:18 +0200

    chore(TRUST): add environment variable for macos

--- products/env_scripts/TRUST.py
+++ products/env_scripts/TRUST.py
@@ -8,7 +8,11 @@ def set_env(env, prereq_dir, version):
     env.set("TRUST_ROOT_DIR",prereq_dir)
     env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
     env.prepend('PATH', os.path.join(prereq_dir, 'include'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     pass
