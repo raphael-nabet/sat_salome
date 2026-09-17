8e52a41c67fbdca09f8bf75c6b2cb5bf92cd09fd
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:02:53 +0200

    chore(Jinja2): add environment variable for macos

--- products/env_scripts/Jinja2.py
+++ products/env_scripts/Jinja2.py
@@ -7,10 +7,18 @@ import platform
 def set_env(env, prereq_dir, version):
     env.set('JINJADIR', prereq_dir)
     env.set('JINJA_ROOT_DIR', prereq_dir) # update for cmake
-    
+
     pyver = 'python' + env.get('PYTHON_VERSION')
 
-    if not platform.system() == "Windows" :
+    if platform.system() == "Windows" :
+        pass
+
+    elif platform.system() == "Darwin" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+
+    else :
         env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
         env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
