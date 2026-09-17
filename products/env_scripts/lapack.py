e58c55c2bdc24c9f304238aa737066c2b131a507
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:07 +0200

    chore(lapack): add environment variable for macos

--- products/env_scripts/lapack.py
+++ products/env_scripts/lapack.py
@@ -5,37 +5,52 @@ import os.path
 import platform
 
 def set_env(env, prereq_dir, version):
-  env.set('LAPACKHOME', prereq_dir)
-  env.set('LAPACK_ROOT_DIR', prereq_dir)
-  env.set('LAPACK_SRC', os.path.join(prereq_dir,'SRC'))
-  env.set('BLAS_SRC', os.path.join(prereq_dir,'BLAS','SRC'))
+    env.set('LAPACKHOME', prereq_dir)
+    env.set('LAPACK_ROOT_DIR', prereq_dir)
+    env.set('LAPACK_SRC', os.path.join(prereq_dir,'SRC'))
+    env.set('BLAS_SRC', os.path.join(prereq_dir,'BLAS','SRC'))
 
-  if not platform.system() == "Windows" :
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    # FOR NUMPY AND SCIPY
-    env.set('BLAS', os.path.join(prereq_dir, 'lib'))
-    env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
-    env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
-    env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
-    env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
-    env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
-    env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
-    env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
-    env.set('CBLAS_ROOT_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
-  else:
-    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
-    env.set('BLAS', os.path.join(prereq_dir, 'lib'))
-    env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
-    env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
-    env.set('lapack_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
-    env.set('lapacke_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
-    env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
-    env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
-    env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
-    env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
-    env.set('cblas_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
-    env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+    if platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+        env.set('BLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
+        env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('lapack_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
+        env.set('lapacke_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
+        env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
+        env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
+        env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
+        env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
+        env.set('cblas_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+        env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # FOR NUMPY AND SCIPY
+        env.set('BLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
+        env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
+        env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
+        env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
+        env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
+        env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+        env.set('CBLAS_ROOT_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+
+    else:
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+        # FOR NUMPY AND SCIPY
+        env.set('BLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
+        env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
+        env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-' + version))
+        env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-' + version))
+        env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
+        env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
+        env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
+        env.set('CBLAS_ROOT_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-' + version))
 
 def set_nativ_env(env):
     env.set('LAPACKHOME', '/usr')
