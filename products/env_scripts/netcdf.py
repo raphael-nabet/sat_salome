1450da1d98396b8bacbb5de8a83872d5b2a0611e
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:04:21 +0200

    chore(netcdf): add environment variable for macos

--- products/env_scripts/netcdf.py
+++ products/env_scripts/netcdf.py
@@ -7,7 +7,11 @@ def set_env(env, prereq_dir, version):
     env.set('NETCDF_INSTALL_DIR', prereq_dir)
     env.set('NETCDF_VERSION',version)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
-    if not platform.system() == "Windows" :
+    if platform.system() == "Windows" :
+        pass
+    elif platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    else:
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
