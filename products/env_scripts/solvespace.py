3387bfe658ea45377c0abc97f04cd7ed4af82491
Raphael Nabet <nabetraphael@icloud.com>
2026-09-17 13:05:09 +0200

    chore(solvespace): add environment variable for macos

--- products/env_scripts/solvespace.py
+++ products/env_scripts/solvespace.py
@@ -4,9 +4,14 @@
 import os.path
 
 def set_env(env, prereq_dir, version):
-    env.set('SOLVESPACE_ROOT_DIR', prereq_dir)
-    root = env.get('SOLVESPACE_ROOT_DIR')
-    env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
+    if platform.system() == "Darwin" :
+        env.set('SOLVESPACE_ROOT_DIR', prereq_dir)
+        root = env.get('SOLVESPACE_ROOT_DIR')
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(root, 'lib'))
+    else:
+        env.set('SOLVESPACE_ROOT_DIR', prereq_dir)
+        root = env.get('SOLVESPACE_ROOT_DIR')
+        env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
 
 def set_nativ_env(env):
     pass
