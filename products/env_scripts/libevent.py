46f961b6dcc05f0fd279145d93a361b955f8585f
Raphael Nabet <nabetraphael@icloud.com>
2026-09-18 15:40:25 +0200

    chore(libevent): add libevent library for openpmix and openmpi

---
+++ products/env_scripts/libevent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os.path, platform
+
+def set_env(env, prereq_dir, version):
+    env.set('LIBEVENTDIR', prereq_dir)
+    env.set('LIBEVENT_ROOT_DIR', prereq_dir)
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    env.prepend('PATH', os.path.join(prereq_dir, 'include'))
+
+    if platform.system() == "Darwin" :
+        env.prepend('DYLD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    else :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+
+def set_nativ_env(env):
+    env.set('LIBEVENTDIR', "/usr")
+    env.set('LIBEVENT_ROOT_DIR', "/usr")
