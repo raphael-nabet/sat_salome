3a636dd873eb7cafcbcae676162954fc4bda43d3
Raphael Nabet <nabetraphael@icloud.com>
2026-09-22 15:06:47 +0200

    chore(libxml2): transitioning the libxml2 build system to cmake from autotools

---
+++ products/compil_scripts/libxml2-2.9.12.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "##########################################################################"
+echo "libxml2" $VERSION
+echo "##########################################################################"
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+CMAKE_OPTIONS=
+# common settings
+CMAKE_OPTIONS+=" -DCMAKE_INSTALL_PREFIX=${PRODUCT_INSTALL}"
+CMAKE_OPTIONS+=" -DCMAKE_VERBOSE_MAKEFILE=ON"
+CMAKE_OPTIONS+=" -DCMAKE_BUILD_TYPE=Release"
+CMAKE_OPTIONS+=" -DENABLE_BUILD_SHARED=ON"
+CMAKE_OPTIONS+=" -DCMAKE_CXX_STANDARD=14"
+
+
+echo "*** cmake" $CMAKE_OPTIONS
+cmake $CMAKE_OPTIONS $SOURCE_DIR
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR on CMake"
+    exit 1
+fi
+
+echo
+echo "*** make" $MAKE_OPTIONS
+make $MAKE_OPTIONS
+if [ $? -ne 0 ]
+then
+    echo "ERROR on make"
+    exit 2
+fi
+
+echo
+echo "*** make install"
+make install
+if [ $? -ne 0 ]
+then
+    echo "ERROR on make install"
+    exit 3
+fi
+
+echo
+echo "########## END"
+
